3bf2c55399f302ec1b00b2c46e4cf7c81a4fbff7
Eugene <eugklim1@gmail.com>
2024-11-16 20:31:40 +0300

    добавлен redirect на home

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: { formats: ['image/avif', 'image/webp'] },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/home',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
